3707096b9a7f782d9dc9719a9d5af2ddaff01433
jamescodegaard <jamescodegaard@gmail.com>
2020-07-27 15:51:09 -0500

    adds map to businesses index - no popups

--- src/views/BusinessesIndex.vue
+++ src/views/BusinessesIndex.vue
@@ -8,10 +8,17 @@
       <p>Phone: {{ business.phone }}</p>
       <router-link :to="`/businesses/${business.id}`">More Info</router-link>
     </div>
+    <div id="map"></div>
   </div>
 </template>
 
 <style>
+#map {
+  top: 0;
+  bottom: 0;
+  height: 500px;
+  width: 100%;
+}
 </style>
 
 <script>
@@ -22,10 +29,18 @@ export default {
       businesses: [],
     };
   },
-  created: function () {
+  mounted: function () {
     axios.get("/api/businesses").then((response) => {
       console.log(response.data);
       this.businesses = response.data;
+    });
+
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_WEB_TOKEN;
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-91.6656, 41.9779], // starting position [lng, lat]
+      zoom: 9, // starting zoom
     });
   },
   methods: {},
